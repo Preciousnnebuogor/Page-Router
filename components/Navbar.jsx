ac9a4aa349c88a5677623be307315edc6a951d84
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-11-13 21:07:05 +0000

    fixed navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,16 @@
-import Link from 'next/link'
-export default function Navbar () {
-    return (
-        <nav>
-         <div className="logo"></div>
-         <div className="list">
-         <h1>Presh List</h1>   
-            </div>
-        <Link href="/">Home</Link> 
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h1 className="logo">Presh List</h1>
+      </div>
+      <div>felix</div>
+      <div className="list">
+        <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/world/presh">World</Link>
-        
-        </nav>
-    )
+      </div>
+    </nav>
+  );
 }
